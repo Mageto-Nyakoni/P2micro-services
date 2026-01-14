15f16a3fe14e75f50ef39eca6b638f104189c7fb
suy3sh <swish.shrestha@gmail.com>
2026-01-13 18:28:26 -0600

    able to login using users from backend

--- backend/smartAppointment/src/main/java/com/revature/smartAppointment/Controller/UserController.java
+++ backend/smartAppointment/src/main/java/com/revature/smartAppointment/Controller/UserController.java
@@ -1,20 +1,22 @@
 package com.revature.smartAppointment.Controller;
 
-import com.revature.smartAppointment.Controller.Request.LoginRequest;
-import com.revature.smartAppointment.Controller.Response.LoginResponse;
-import com.revature.smartAppointment.Model.User;
-
-import com.revature.smartAppointment.Service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.smartAppointment.Model.User;
+import com.revature.smartAppointment.Service.UserService;
+
 @RestController
 @RequestMapping("/smart-appointment/api/users")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private UserService userService;
