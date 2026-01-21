f7da5548407b22f9b9983e9acc6e1a1a961f2017
acmullin17 <acmullin17@gmail.com>
2026-01-21 16:02:38 -0600

    weird expanded imports fix

--- backend/smartAppointment/src/main/java/com/revature/smartAppointment/Model/Patient.java
+++ backend/smartAppointment/src/main/java/com/revature/smartAppointment/Model/Patient.java
@@ -4,38 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "patient")
@@ -68,7 +38,7 @@ public class Patient {
     @JoinColumn(name = "blood_type_id")
     private BloodType bloodType;
 
-    @ManyToMany(fetch = FetchType.LAZY)(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "patient_allergies",
             joinColumns = @JoinColumn(name = "patient_id"),
--- backend/smartAppointment/src/main/java/com/revature/smartAppointment/Model/Speciality.java
+++ backend/smartAppointment/src/main/java/com/revature/smartAppointment/Model/Speciality.java
@@ -3,20 +3,8 @@ package com.revature.smartAppointment.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "speciality")
@@ -36,7 +24,7 @@ public class Speciality {
     @Column(name = "description")
     private String description;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "speciality_appointment_type",
             joinColumns = @JoinColumn(name = "speciality_id"),
