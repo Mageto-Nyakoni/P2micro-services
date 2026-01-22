f966bbb8dc9fe7d99a32d35902ba00eb784f32c8
acmullin17 <acmullin17@gmail.com>
2026-01-22 09:34:49 -0600

    small change to super home

--- frontend/appointment-frontend/src/pages/super/SuperHome.tsx
+++ frontend/appointment-frontend/src/pages/super/SuperHome.tsx
@@ -1,3 +1,10 @@
 export default function SuperHome() {
-  return <div className="p-6 text-xl font-semibold">Super: Home</div>;
+
+  return (
+    <div className="p-6 min-h-screen bg-slate-50">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold">Super Dashboard</h1>
+      </div>
+    </div>
+  );
 }
