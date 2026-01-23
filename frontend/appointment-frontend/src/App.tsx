3317c5b87d3286a4264c2c5d5d572657c056156e
Mageto-Nyakoni <91837203+Mageto-Nyakoni@users.noreply.github.com>
2026-01-23 16:17:36 -0600

    reverted application.properties file, added the right file path to App for Admin User

--- frontend/appointment-frontend/src/App.tsx
+++ frontend/appointment-frontend/src/App.tsx
@@ -18,7 +18,7 @@ import EditPatientProfile from "./pages/patient/EditPatientProfile";
 
 import DoctorHome from "./pages/doctor/DoctorHome";
 import DoctorCalendar from "./pages/doctor/DoctorCalendar";
-import AdminSchedule from "./pages/admin/AdminSchedule";
+import AdminHome from "./pages/admin/AdminHome";
 import AdminStaffList from "./pages/admin/AdminStaffList";
 import Navbar from "./components/layout/NavBar/Navbar";
 import SuperHome from "./pages/super/SuperHome";
@@ -80,7 +80,7 @@ function App() {
             {/* Admin Protected Routes */}
             <Route path="/admin/home" element={
               <ProtectedRoute allowedRoles={["Admin"]}>
-                <AdminSchedule />
+                <AdminHome />
               </ProtectedRoute>
               } />
             <Route path="/admin/staff" element={
