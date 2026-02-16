848b7d413396e458002eb8fed945b7a8e5de84f2
annsusan28 <annsusan2823@gmail.com>
2026-02-16 11:02:51 -0600

    updated the appointment service

--- AppointmentService/src/main/java/com/revature/dto/response/AppointmentDto.java
+++ AppointmentService/src/main/java/com/revature/dto/response/AppointmentDto.java
@@ -1,5 +1,12 @@
 package com.revature.dto.response;
+import java.time.LocalDateTime;  
+import com.revature.model.AppointmentStatus;
+public record AppointmentDto(
+    Integer appointmentId,
+    String doctorName,
+    String appointmentType,
+    LocalDateTime startTime,
+    LocalDateTime endTime,
+    AppointmentStatus status
+) {}
 
-public class AppointmentDto {
-    
-}
